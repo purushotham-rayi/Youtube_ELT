503ac3c42b3d3c86c2698e1d9f3b592fb0ad7d25
purushotham-rayi <rayipurushotham98799@gmail.com>
2026-01-22 15:57:04 -0600

    Modified the video_stats.py code to get playlist_ID

--- video_stats.py
+++ video_stats.py
@@ -3,37 +3,32 @@ import json
 import os
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path="./env")
+load_dotenv(dotenv_path=".env")
 
 API_KEY=os.getenv("API_KEY")
 CHANNEL_HANDLE="MrBeast"
-url=f"https://youtube.googleapis.com/youtube/v3/channels?part=statistics&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+url=f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+
 
 def get_playlist_id():
-    print("Funtion Started.............")
     try:
-        print("Making API Requests......")
         response=requests.get(url)
-        print(f"Response Status: {response.status_code}")
-
         data=response.json()
-        print('JSON parsed Successfully................')
 
-        json_data=json.dumps(data,indent=4)
-        print(json_data)
+        response.raise_for_status()
+        # json_data=json.dumps(data,indent=4)
+        # print(json_data)
 
-        # with open('video_stats.json','w') as f:
-        #     f.write(json_data)
-
-        channel_items=data['items'][0]
-        # channel_playlistId=channel_items['contentDetails']['relatedPlaylists']['uploads']
-        print(channel_items)
-        return channel_items
+        channel_items=data["items"][0]
+        channel_playlist_id=channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
+        # print(channel_playlist_id)
+        return channel_playlist_id
     except requests.exceptions.RequestException as e:
         print(f"Error occured: {e}")
         raise e
     
 if __name__ == "__main__":
-    print("Starting Program..................")
-    get_playlist_id()
-    print("...........................\n Program finished")
+    print("get_playlist_id will be executed")
+    print(get_playlist_id())
+else:
+    print("get_playlist_id will be executed")
