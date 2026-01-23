74efe3677e494b35bce91c472dd6d729f8e5a9b4
purushotham-rayi <rayipurushotham98799@gmail.com>
2026-01-23 14:35:57 -0600

    Added the funtion to get the videoId for all the videos on the channel

--- video_stats.py
+++ video_stats.py
@@ -8,6 +8,7 @@ load_dotenv(dotenv_path=".env")
 API_KEY=os.getenv("API_KEY")
 CHANNEL_HANDLE="MrBeast"
 url=f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+maxResults=50
 
 
 def get_playlist_id():
@@ -26,9 +27,31 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         print(f"Error occured: {e}")
         raise e
-    
+
+def get_video_ids(playlist_ID):
+    video_ids=[]
+    pageToken = None
+    base_url=f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlist_ID}&key={API_KEY}"
+    try:
+        while True:
+            url=base_url
+            if pageToken:
+                url+=f"&pageToken={pageToken}"
+            response = requests.get(url)
+            response.raise_for_status()
+            data=response.json()
+
+            for item in data.get('items',[]):
+                video_id=item['contentDetails']['videoId']
+                video_ids.append(video_id)
+            pageToken=data.get('nextPageToken')
+            if not pageToken:
+                break
+    except requests.exceptions.RequestException as e:
+        raise e
+    return video_ids
+
+playlist_ID = get_playlist_id()
 if __name__ == "__main__":
-    print("get_playlist_id will be executed")
-    print(get_playlist_id())
-else:
-    print("get_playlist_id will be executed")
+    playlist_ID=get_playlist_id()
+    get_video_ids(playlist_ID)
