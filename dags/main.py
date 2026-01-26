de6c2e43aa1111ae7b0d90e71ac9ad3c60a3e54d
purushotham-rayi <rayipurushotham98799@gmail.com>
2026-01-25 23:28:12 -0600

    Added the script for dag running and scheduling

--- dags/main.py
+++ dags/main.py
@@ -3,6 +3,8 @@ import pendulum
 from datetime import datetime, timedelta
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
 
+from datawarehouse.dwh import staging_table, core_table
+
 # Define the local timezone
 local_tz = pendulum.timezone("America/Chicago")
 
@@ -24,7 +26,7 @@ with DAG(
      dag_id="produce_json",
      default_args=default_args,
      description='DAG produce JSON file with raw data',
-     schedule="0 14 * * *",
+     schedule="00 12 * * *",
      catchup = True
 ) as dag:
 
@@ -37,3 +39,19 @@ with DAG(
     # Define dependencies
 
     playlist_id >> video_ids >> extract_data >> save_to_json_task
+
+with DAG(
+     dag_id="update_db",
+     default_args=default_args,
+     description='DAG to process JSON file and insert data into both staging and core schemas',
+     schedule="30 12 * * *",
+     catchup = False
+    ) as dag:
+
+    # Define tasks
+    update_staging = staging_table()
+    update_core = core_table()
+
+    # Define dependencies
+
+    update_staging >> update_core
